640726bfb04f307728e9ae304e7489794b3caff9
Marcus <marcussampaio123@hotmail.com>
2022-08-01 00:27:50 -0300

    terminando a aplicação do challenger

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,30 +1,46 @@
 <template>
-  <div class="container">
-    <h4>WEATHER</h4>
+  <div class="container" @keyup.enter="getLocationName">
+    <header>
+      <h4>WEATHER</h4>
+    </header>
     <div class="logo">
       <q-icon name="cloud" size="70px" class="q-icon-cloud" />
     </div>
-    <span>select a city</span>
+    <span>Select a city</span>
     <span class="textExamples">
       Examples: Recife, Vancouver, London, Dallol, Fairbanks, Yakutsk
     </span>
     <q-card class="my-card">
       <q-card-section>
         <q-input type="text" v-model="cityName" label="Write name city" />
-        <div class="testeSeparando">
-          <q-btn label="Search" color="accent" @click="getLocationName" />
-          <q-btn label="Clear" color="accent" @click="clearFields" />
+        <div class="btn-buttons">
+          <q-btn
+            outline
+            label="Search"
+            color="accent"
+            @click="getLocationName"
+          />
+          <q-btn outline label="Clear" color="accent" @click="clearFields" />
         </div>
       </q-card-section>
     </q-card>
   </div>
-  <div v-if="showAllData" class="containerData">
+  <div v-if="showIconWeather" class="iconWeather">
     <img :src="icon" size="500" />
-    <p>País: {{ data.location.country }}</p>
-    <p>Cidade: {{ data.location.name }}</p>
-    <p>Estado: {{ data.location.region }}</p>
-    <p>Temperatura: {{ data.current.temp_c }}°C</p>
-    <p>Umidade: {{ data.current.humidity }}%</p>
+  </div>
+  <div v-if="showAllData" class="containerData">
+    <div>
+      <p>Country: {{ data.location.country }}</p>
+      <p>State: {{ data.location.region }}</p>
+      <p>City: {{ data.location.name }}</p>
+    </div>
+    <div>
+      <p>
+        Temperature: {{ data.current.temp_c }}°C / {{ data.current.temp_f }}°F
+      </p>
+      <p>Air Humidity: {{ data.current.humidity }}%</p>
+      <p>Date/Time: {{ data.location.localtime }}</p>
+    </div>
   </div>
 </template>
 
@@ -36,33 +52,32 @@ export default defineComponent({
   name: "Home",
   setup() {
     const showAllData = ref(false);
-    const weather = ref([]);
+    const showIconWeather = ref(false);
     const cityName = ref("");
-    const region = ref("");
     const data = ref([]);
     const icon = ref("");
 
     async function getLocationName() {
       await api.get(`${cityName.value}`).then((response) => {
-        weather.value = response.data;
-        region.value = response.data.location.region;
         cityName.value = response.data.location.name;
         data.value = response.data;
         icon.value = response.data.current.condition.icon;
       });
       if (data != null) {
         showAllData.value = true;
+        showIconWeather.value = true;
       }
     }
     async function clearFields() {
       showAllData.value = false;
+      showIconWeather.value = false;
       cityName.value = "";
     }
+    
     return {
-      weather,
       cityName,
-      region,
       showAllData,
+      showIconWeather,
       data,
       icon,
       getLocationName,
@@ -88,29 +103,54 @@ export default defineComponent({
   align-items: center;
   background-color: rgb(0, 0, 0, 0.5);
   color: #fff;
+  height: 60vh;
+}
+
+.iconWeather {
+  background-color: rgb(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 100px;
+  padding: 3px;
+}
+
+.forms {
+  width: 25px;
 }
 
 .textExamples {
-  font-size: 13px;
+  font-size: 15px;
+  font-weight: bold;
+  margin: 3px;
+  padding: 2px;
+}
+
+span,
+p {
+  font-weight: bold;
+  font-size: 20px;
 }
 
 .logo {
   text-align: center;
 }
-.testeSeparando {
+.btn-buttons {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-end;
+  padding: 5px;
+  margin: 5px;
 }
 
 .containerData {
   background-color: rgb(0, 0, 0, 0.5);
   color: #fff;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  height: 57vh;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  height: 25vh;
 }
 </style>
